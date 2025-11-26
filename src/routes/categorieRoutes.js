53e5c44ef12a60d67a067d45cc9cc80cf284443d
lantomalala <lanraybeljustin@gmail.com>
2025-11-26 13:35:49 +0300

    feat: add functionnality to get all link in categorie

--- src/routes/categorieRoutes.js
+++ src/routes/categorieRoutes.js
@@ -1,59 +1,59 @@
-import express from "express";
-import * as productService from "../services/productService.js";
-import axios from "axios";
+import express from 'express';
+import * as productService from '../services/productService.js';
+import axios from 'axios';
 
 const router = express.Router();
+const pushOne = async url => {
+    try {
+        const urlToScrape = url; // ou une URL fixe
+        const response = await axios.get(`http://taapit-scraping-api-etyf.onrender.com/api/ebay/product?path=${encodeURIComponent(urlToScrape)}`, {
+            maxBodyLength: Infinity,
+            headers: {}
+        });
+
+        const productData = response.data;
+
+        // Mapper le JSON reçu vers les champs Prisma
+        const productToCreate = {
+            itemId: productData.itemId,
+            title: productData.title,
+            oemReference: productData.oemReference,
+            priceNet: parseFloat(productData.price?.net_price) || null,
+            priceBrut: parseFloat(productData.price?.brut_price) || null,
+            currency: productData.price?.currency || null,
+            url: productData.url,
+            images: productData.images || [],
+            seller: productData.seller || null,
+            listingStartDate: productData.listingStartDate ? new Date(productData.listingStartDate) : null,
+            status: productData.status || 'ACTIVE'
+        };
+
+        const product = await productService.createProduct(productToCreate);
+
+        // utilisation
+        console.log('push done pour: ' + product.itemId);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({error: e.message});
+    }
+};
 
 // Créer un produit
-router.post("/product", async (req, res) => {
-  try {
-    const urlToScrape = req.body.url || req.query.url; // ou une URL fixe
-    const response = await axios.get(
-      `http://taapit-scraping-api-etyf.onrender.com/api/ebay/product?path=${encodeURIComponent(
-        urlToScrape
-      )}`,
-      {
-        maxBodyLength: Infinity,
-        headers: {},
-      }
-    );
+router.post('/', async (req, res) => {
+    try {
+        const urlToScrape = req.body.categorie || req.query.categorie; // ou une URL fixe
+        const response = await axios.get(`http://taapit-scraping-api-etyf.onrender.com/api/ebay/categorie?path=${encodeURIComponent(urlToScrape)}`, {
+            maxBodyLength: Infinity,
+            headers: {}
+        });
 
-    const productData = response.data;
+        const product_links = await response;
 
-    // Mapper le JSON reçu vers les champs Prisma
-    const productToCreate = {
-      itemId: productData.itemId,
-      title: productData.title,
-      oemReference: productData.oemReference,
-      priceNet: parseFloat(productData.price?.net_price) || null,
-      priceBrut: parseFloat(productData.price?.brut_price) || null,
-      currency: productData.price?.currency || null,
-      url: productData.url,
-      images: productData.images || [],
-      seller: productData.seller || null,
-      listingStartDate: productData.listingStartDate
-        ? new Date(productData.listingStartDate)
-        : null,
-      status: productData.status || "ACTIVE",
-    };
-
-    const product = await productService.createProduct(productToCreate);
-
-    function serializeBigInt(obj) {
-        return JSON.parse(JSON.stringify(obj, (_, value) =>
-            typeof value === 'bigint' ? value.toString() : value
-        ));
+        res.json(product_links);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({error: e.message});
     }
-    
-    // utilisation
-    res.json(serializeBigInt(product));    
-    
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  }
 });
-
-
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
+import categorieRoutes from './routes/categorieRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,5 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/categorie', categorieRoutes);
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
